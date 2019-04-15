f800d27cc8e9e782bdc24f3ce4f7f38ac90aca58
daifeil <30922155+daifeil@users.noreply.github.com>
2019-04-15 16:18:08 +0800

    Update APPBackgroundMode.m

--- src/ios/APPBackgroundMode.m
+++ src/ios/APPBackgroundMode.m
@@ -264,8 +264,7 @@ NSString* const kAPPBackgroundEventDeactivate = @"deactivate";
 
 //         [obj setValue:[NSNumber numberWithBool:YES]
 //                forKey:[APPBackgroundMode wkProperty]];
-        [obj setValue:[NSNumber numberWithBool:NO]
-               forKey:@"requiresUserActionForMediaPlayback"];
+       
         [obj setValue:[NSNumber numberWithBool:NO]
                forKey:@"requiresUserActionForMediaPlayback"];
 
